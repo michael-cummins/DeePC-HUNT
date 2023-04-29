e4f5f521ba98b973fa44fc1bbdf1ae1968cdab91
Michael Cummins <michaelcummins@Michaels-MacBook-Air.local>
2023-04-29 14:35:04 +0200

    Moving to MPC cartpole dx

--- src/controller_utils.py
+++ src/controller_utils.py
@@ -18,13 +18,22 @@ def block_hankel(w: np.array, L: int, d: int) -> np.array:
         H[:,i] = w[d*i:d*(L+i)]
     return H
 
+def block_hankel_torch(w: torch.Tensor, L: int, d: int) -> torch.Tensor:
+    T = int(len(w)/d)
+    if L > T:
+        raise ValueError('L must be smaller than T')
+    H = torch.zeros((L*d, T-L+1))
+    for i in range(0, T-L+1):
+        H[:,i] = w[d*i:d*(L+i)]
+    return H
+
 class Clamp(torch.autograd.Function):
     """
     https://discuss.pytorch.org/t/regarding-clamped-learnable-parameter/58474/4
     """
     @staticmethod
     def forward(ctx, input):
-        return input.clamp(min=1e-4, max=1e5) # the value in iterative = 2
+        return input.clamp(min=1e-4, max=1e9) # the value in iterative = 2
 
     @staticmethod
     def backward(ctx, grad_output):
